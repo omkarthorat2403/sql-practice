f23bb1fcd168b53060ab6e831c85b6ffe7a13584
omkarthorat2403 <omkarthorat2403@gmail.com>
2026-04-15 22:12:17 +0530

    updated sql queries

--- sql class day2.sql
+++ sql class day2.sql
@@ -23,18 +23,21 @@ select * from student;
 insert into student(name,mail,mobile) values ('om','om@gmail.com','999786789');
 insert into student(name,mail,mobile) values ('raj','raj123@gmail.com','7874219800');
 
--- 1.insert without coloumn name(order matters)
+-- 2.insert without coloumn name(order matters)
 insert into student values ('ganesh','ganesh2003@gmail.com','776539800');
 insert into student values ('prem','prem22@gmail.com','790839800');
 
--- 1.insert with multiple rows(bulk insert)
+-- 3.insert with multiple rows(bulk insert)
 insert into student values
 ('rajesh','raju22@gmail.com','98657833390'),
 ('anant','anant1996@gmail.com','860531248'),
 ('rajesh','raju22@gmail.com','98657833390');
 
--- 1.insert into selected coloumn
+-- 4.insert into selected coloumn
 insert into student(name,mobile) values
 ('arati','909783330');
 
 select * from student;
+
+desc student;
+
